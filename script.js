2cd7512dc855c9596fe41d2b6ca92ebe949e884b
Yash Sancheti <32770175+Onyx2406@users.noreply.github.com>
2022-04-15 20:27:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// scroll-to-top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+	window.scrollTo({
+	  top: 0,
+	  left: 0,
+	  behavior: "smooth",
+	});
+  });
+
 var winWidth = $(window).width();
 	dropdownMenu(winWidth);
 	
